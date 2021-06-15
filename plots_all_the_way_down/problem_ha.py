150c2c6f2157540b90d9cf6b62a7050cae0109ef
Hollis Akins <hollis.akins@gmail.com>
2021-06-15 12:40:55 -0500

    plot beautification

--- plots_all_the_way_down/problem_ha.py
+++ plots_all_the_way_down/problem_ha.py
@@ -12,7 +12,15 @@ spec_sed = spec['flux_spec'] #jansky
 phot_wav = phot['filter_lambda']
 phot_sed = phot['flux_mag']
 
-plt.title('Fix me, pleaase!')
-plt.loglog(spec_wav, spec_sed)
-plt.scatter(phot_wav, phot_sed)
+fig, ax = plt.subplots(1,1,dpi=300)
+ax.set_title('Fixed!')
+ax.plot(spec_wav, spec_sed, color='k', linewidth=1, label='SED')
+ax.scatter(phot_wav, phot_sed, color='r', label='Photometry')
+ax.set_ylabel(r'Flux [Jy]')
+ax.set_xlabel(r'Wavelength [$\mu$m]')
+ax.loglog()
+ax.set_xlim(8e1, 2e7)
+ax.set_ylim(1e4, 1e14)
+ax.grid(alpha=0.1, zorder=-1000)
+ax.legend(frameon=False)
 plt.show()
