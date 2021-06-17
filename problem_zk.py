72bc56935b60a8a7800bbcd6a2ab5d593816293d
Zoe_Kearney <zoe.kearney@cfa.harvard.edu>
2021-06-17 18:02:09 -0400

    fixed

--- problem_zk.py
+++ problem_zk.py
@@ -1,7 +1,7 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
 
-
+fig =plt.figure()
 spec = pd.read_csv('example_spec_SED.csv')
 phot = pd.read_csv('example_phot_SED.csv')
 
@@ -12,7 +12,11 @@ spec_sed = spec['flux_spec'] #jansky
 phot_wav = phot['filter_lambda']
 phot_sed = phot['flux_mag']
 
-plt.title('Fix me, pleaase!')
-plt.loglog(spec_wav, spec_sed)
-plt.scatter(phot_wav, phot_sed)
+plt.title('SED')
+plt.loglog(spec_wav, spec_sed,c='k', label ='Spectroscopic data')
+plt.scatter(phot_wav, phot_sed,marker='*',color='r',label='Photometric data')
+plt.xlabel('Wavlength [$\AA$]')
+plt.ylabel('Flux [Jy]')
+plt.legend()
 plt.show()
+fig.savefig('pretty_sed.png',dpi=300)
